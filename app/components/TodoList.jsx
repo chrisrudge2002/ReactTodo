7cb97f72975401c67ebab32dc32183959ba658db
Chris Rudge <chrisrudge2002@gmail.com>
2017-02-04 15:03:04 +0000

    Add toggle support and tests

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -7,7 +7,7 @@ const TodoList = React.createClass({
 		const {todos} = this.props;
 		const renderTodos = () => {
 			return todos.map((todo) => {
-				return (<Todo key={todo.id} {...todo}/>);
+				return (<Todo key={todo.id} {...todo} onToggle={this.props.onToggle}/>);
 			});
 		};
 
