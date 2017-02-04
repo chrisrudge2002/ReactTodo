b7edf3da733dfc21b852c933bef372677c7f6e57
Chris Rudge <chrisrudge2002@gmail.com>
2017-02-04 14:09:40 +0000

    Add Search component

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -1,10 +1,24 @@
 const React = require('react');
 
 const Search = React.createClass({
+	handleSearch: function() {
+		const showCompleted = this.refs.showCompleted.checked;
+		const searchText = this.refs.searchText.value;
+
+		this.props.onSearch(showCompleted, searchText);
+	},
 	render: function() {
 		return (
 			<div>
-
+				<div>
+					<input type="search" ref="searchText" placeholder="Search todos" onChange={this.handleSearch}/>
+				</div>
+				<div>
+					<label>
+						<input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
+						Show completed todos
+					</label>
+				</div>
 			</div>
 		);
 	}
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,11 +1,14 @@
 const React = require('react');
 
 const AddTodo = require('AddTodo');
+const Search = require('Search');
 const TodoList = require('TodoList');
 
 const TodoApp = React.createClass({
 	getInitialState: function() {
 		return {
+			searchText: '',
+			showCompleted: false,
 			todos: [
 				{
 					id: 1,
@@ -24,13 +27,20 @@ const TodoApp = React.createClass({
 		};
 	},
 	handleAddTodo: function(text) {
-		alert(text);
+		
+	},
+	handleSearch: function(showCompleted, searchText) {
+		this.setState({
+			searchText: searchText.toLowerCase(),
+			showCompleted: showCompleted
+		});
 	},
 	render: function() {
 		const {todos} = this.state;
 
 		return (
 			<div>
+				<Search onSearch={this.handleSearch}/>
 				<TodoList todos={todos}/>
 				<AddTodo onAddTodo={this.handleAddTodo}/>
 			</div>
