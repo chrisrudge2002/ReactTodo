a32749efa49da69c544ce22791c62692456c67fd
Chris Rudge <chrisrudge2002@gmail.com>
2017-02-04 14:20:08 +0000

    Add Search component tests

--- app/tests/components/Search.test.jsx
+++ app/tests/components/Search.test.jsx
@@ -10,4 +10,25 @@ describe('Search', () => {
 	it('should exist', () => {
 		expect(Search).toExist();
 	});
+
+	it('should call onSearch with entered input text', () => {
+		const searchText = 'Dog';
+		const spy = expect.createSpy();
+		const search = TestUtils.renderIntoDocument(<Search onSearch={spy}/>);
+		
+		search.refs.searchText.value = searchText;
+		TestUtils.Simulate.change(search.refs.searchText);
+
+		expect(spy).toHaveBeenCalledWith(false, searchText);
+	});
+
+	it('should call onSearch with proper checked value', () => {
+		const spy = expect.createSpy();
+		const search = TestUtils.renderIntoDocument(<Search onSearch={spy}/>);
+		
+		search.refs.showCompleted.checked = true;
+		TestUtils.Simulate.change(search.refs.showCompleted);
+
+		expect(spy).toHaveBeenCalledWith(true, '');
+	});
 });
